2d413487cd45769c32398f9f8355f43ca0357916
Alexey Korshun <lehakorshun@gmail.com>
2019-02-03 15:15:53 +0600

    provide ui scope and bg scope in app presenter

--- app/src/main/java/com/rosberry/android/lastfm/base/presentation/AppPresenter.kt
+++ app/src/main/java/com/rosberry/android/lastfm/base/presentation/AppPresenter.kt
@@ -8,10 +8,21 @@ package com.rosberry.android.lastfm.base.presentation
 
 import com.arellomobile.mvp.MvpPresenter
 import com.arellomobile.mvp.MvpView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 
 /**
  * @author Alexei Korshun on 03/02/2019.
  */
 open class AppPresenter<V : MvpView> : MvpPresenter<V>() {
 
+    protected val job = SupervisorJob()
+    protected val uiScope = CoroutineScope(Dispatchers.Main + job)
+    protected val bgScope = CoroutineScope(Dispatchers.IO + job)
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
+    }
 }
