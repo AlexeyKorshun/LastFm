141b7aa00e3945d7e27b6e22dece6601556eebe9
Alexey Korshun <lehakorshun@gmail.com>
2019-02-03 11:27:49 +0600

    create search screen

---
+++ app/src/main/java/com/rosberry/android/lastfm/ui/main/search/SearchFragment.kt
@@ -0,0 +1,17 @@
+/*
+ *
+ *  * Copyright (c) 2018 Rosberry. All rights reserved.
+ *
+ */
+
+package com.rosberry.android.lastfm.ui.main.search
+
+import com.rosberry.android.lastfm.R
+import com.rosberry.android.lastfm.base.ui.AppFragment
+
+/**
+ * @author Alexei Korshun on 03/02/2019.
+ */
+class SearchFragment : AppFragment() {
+    override val layoutRes: Int = R.layout.fragment_search
+}
