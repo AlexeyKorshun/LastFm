0738e4219f8c88b8d854dc700831cc4e9aee2ecd
Alexey Korshun <lehakorshun@gmail.com>
2019-02-03 09:17:17 +0600

    rename main activity, remove tests

--- app/src/main/java/com/rosberry/android/lastfm/LaunchActivity.kt
+++ app/src/main/java/com/rosberry/android/lastfm/LaunchActivity.kt
@@ -3,7 +3,7 @@ package com.rosberry.android.lastfm
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class LaunchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
