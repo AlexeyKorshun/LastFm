2720f38e0ebfad372e9ba9a9d4cae0e904ecd6eb
Alexey Korshun <lehakorshun@gmail.com>
2019-02-03 11:28:07 +0600

    create screens

--- app/src/main/java/com/rosberry/android/lastfm/Screens.kt
+++ app/src/main/java/com/rosberry/android/lastfm/Screens.kt
@@ -8,6 +8,8 @@ package com.rosberry.android.lastfm
 
 import androidx.fragment.app.Fragment
 import com.rosberry.android.lastfm.ui.main.MainFragment
+import com.rosberry.android.lastfm.ui.main.favorites.FavoritesFragment
+import com.rosberry.android.lastfm.ui.main.search.SearchFragment
 import ru.terrakok.cicerone.android.support.SupportAppScreen
 
 /**
@@ -18,5 +20,15 @@ object Screens {
     class MainScreen : SupportAppScreen() {
 
         override fun getFragment(): Fragment = MainFragment()
+    }
+
+    class FavoritesScreen : SupportAppScreen() {
+
+        override fun getFragment(): Fragment = FavoritesFragment()
+    }
+
+    class SearchScreen : SupportAppScreen() {
+
+        override fun getFragment(): Fragment = SearchFragment()
     }
 }
