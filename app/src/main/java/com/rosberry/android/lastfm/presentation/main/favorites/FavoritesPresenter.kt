e0590ab34685dca2d6a7392e6a9014b85173e009
Alexey Korshun <lehakorshun@gmail.com>
2019-02-03 11:27:33 +0600

    create favorites screen

---
+++ app/src/main/java/com/rosberry/android/lastfm/presentation/main/favorites/FavoritesPresenter.kt
@@ -0,0 +1,18 @@
+/*
+ *
+ *  * Copyright (c) 2018 Rosberry. All rights reserved.
+ *
+ */
+
+package com.rosberry.android.lastfm.presentation.main.favorites
+
+import com.arellomobile.mvp.InjectViewState
+import com.arellomobile.mvp.MvpView
+import com.rosberry.android.lastfm.base.presentation.AppPresenter
+
+/**
+ * @author Alexei Korshun on 03/02/2019.
+ */
+@InjectViewState
+class FavoritesPresenter : AppPresenter<MvpView>() {
+}
